5f557643c3191e779dd15a5879c811ea0819b300
Mehrdad Shokri <code.poet95@gmail.com>
2020-03-12 14:19:14 +0330

    WIP setting up boilerplate + drawer implementation

---
+++ Navigator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import HomeScreen from './screens/Home';
+const Drawer = createDrawerNavigator();
+import DrawerContent from './components/Drawer'
+
+
+export const RootNavigator = () => {
+  return (
+    <Drawer.Navigator drawerContent={() => <DrawerContent />}>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export default RootNavigator;
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Navigator from './Navigator';
 import {name as appName} from './app.json';
+import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+import FeatherIcons from 'react-native-vector-icons/SimpleLineIcons';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  },
+};
+const AppWrapper = () => (
+  <PaperProvider
+  /*settings={{
+           icon: props => <FeatherIcons {...props} />,
+         }}*/
+  >
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  </PaperProvider>
+);
+AppRegistry.registerComponent(appName, () => AppWrapper);
